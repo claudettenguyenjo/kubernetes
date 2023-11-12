d942e6eedc8b1d5e3f82e1f268ab24849f2e5ac6
claudettenguyenjo <138328890+claudettenguyenjo@users.noreply.github.com>
2023-11-12 17:27:06 +0200

    Update admission_test.go

--- plugin/pkg/admission/admit/admission_test.go
+++ plugin/pkg/admission/admit/admission_test.go
@@ -27,14 +27,6 @@ import (
 )
 
 
-func TestAdmissionNilAttribute(t *testing.T) {
-	handler := NewAlwaysAdmit()
-	err := handler.(*alwaysAdmit).Admit(context.TODO(), nil, nil)
-	if err != nil {
-		t.Errorf("Unexpected error returned from admission handler")
-	}
-}
-
 func TestHandles(t *testing.T) {
 	handler := NewAlwaysAdmit()
 	tests := []admission.Operation{admission.Create, admission.Connect, admission.Update, admission.Delete}
